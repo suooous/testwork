ec4a91862c54827cf565c585c1173b2d30438c90
suooous <1934116641@qq.com>
2025-01-21 16:51:07 +0800

    first

---
+++ project1.py
@@ -0,0 +1,15 @@
+from sortedcontainers import SortedList
+nums = [1,-1,-3,-2,3]
+k = 3
+x = 2
+
+ls=SortedList(nums[:k-1])
+ans=[]
+for i in range (len(nums)-k+1):
+    ls.add(nums[i+k-1])
+    v=ls[x-1]
+    if v>=0:
+        ans.append(0)
+    else :
+        ans.append(v)
+    ls.discard(nums[i])
